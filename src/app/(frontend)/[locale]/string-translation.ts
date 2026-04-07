198d077555436ddea0b29396ef59144a9ab9e49c
Krishan Br. Chand <krishna@pigment.se>
2026-04-07 11:58:11 +0545

    add 404 page

---
+++ src/app/(frontend)/[locale]/string-translation.ts
@@ -0,0 +1,14 @@
+export const notFoundString = {
+  title: {
+    ne: '४०४',
+    en: '404',
+  },
+  message: {
+    ne: 'यो पृष्ठ फेला पार्न सकिएन।',
+    en: 'This page could not be found.',
+  },
+  buttonText: {
+    ne: 'होम पृष्ठ जानुहोस्',
+    en: 'Go to home',
+  },
+}
